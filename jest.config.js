907dc0ce65f0ad2fd0ef2b13684d0773a900bde2
Minseok Kim <mskim@ridi.com>
2019-07-17 12:23:00 +0900

    Enhance project stability
    - Fix vulnerabilities
    - Ignore dist directory from the test paths
    - Upgrade old packages

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,11 @@ module.exports = {
   "transform": {
     "^.+\\.tsx?$": "ts-jest"
   },
-
   "testRegex": "/__tests__/.*\\.ts$",
+  "testPathIgnorePatterns": [
+    "<rootDir>/node_modules/",
+    "<rootDir>/dist/"
+  ],
   "moduleFileExtensions": [
     "ts",
     "tsx",
